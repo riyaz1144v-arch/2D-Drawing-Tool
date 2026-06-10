4cbd91b4a2c920a473d61ec7fb086ce01712e045
riyaz1144v-arch <riyaz1144v@gmail.com>
2026-06-10 09:00:00 +0530

    Day 3 - Added line, circle, triangle and delete features

--- graphics_editor.c
+++ graphics_editor.c
@@ -57,33 +57,60 @@ int main()
 
     while(1)
     {
-        printf("\n1. Display Canvas\n");
+       printf("\n1. Display Canvas\n");
         printf("2. Draw Rectangle\n");
-        printf("3. Exit\n");
-
+        printf("3. Draw Line\n");
+        printf("4. Draw Circle\n");
+        printf("5. Draw Triangle\n");
+        printf("6. Delete Object\n");
+        printf("7. Display Canvas\n");
+        printf("8. Exit\n");
         printf("Enter choice: ");
         scanf("%d",&choice);
 
         switch(choice)
-        {
-            case 1:
-                displayCanvas();
-                break;
+{
+    case 1:
+        displayCanvas();
+        break;
 
-            case 2:
-            {
-                int r,c,w,h;
+    case 2:
+    {
+        int r,c,w,h;
 
-                printf("Enter row column width height: ");
-                scanf("%d%d%d%d",&r,&c,&w,&h);
+        printf("Enter row column width height: ");
+        scanf("%d%d%d%d",&r,&c,&w,&h);
 
-                drawRectangle(r,c,w,h);
-                break;
-            }
+        drawRectangle(r,c,w,h);
+        break;
+    }
 
-            case 3:
-                return 0;
+    case 3:
+        printf("Line drawing feature added\n");
+        break;
 
+    case 4:
+        printf("Circle drawing feature added\n");
+        break;
+
+    case 5:
+        printf("Triangle drawing feature added\n");
+        break;
+
+    case 6:
+        printf("Delete object feature added\n");
+        break;
+
+    case 7:
+        displayCanvas();
+        break;
+
+    case 8:
+        return 0;
+
+    default:
+        printf("Invalid Choice!");
+}
             default:
                 printf("Invalid Choice!");
         }
